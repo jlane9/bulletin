aec4fb50a2f5362d275fe4d2858d0272c2a6e5a7
John Lane <john.lane93@gmail.com>
2020-11-09 12:03:40 -0600

    Make password a write only field

--- bulletin/serializers.py
+++ bulletin/serializers.py
@@ -6,10 +6,22 @@ class UserSerializer(serializers.ModelSerializer):
     """User REST serializer
     """
 
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data: dict):
+        """Create new user
+        """
+
+        user = User.objects.create(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
     class Meta:
         """User REST serializer metadata
         """
 
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'date_joined')
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'date_joined')
         read_only_fields = ('id',)
